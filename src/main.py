50c77824aacc7eb2f0df5c4ad661a3f4bd1f6b24
goutham-soratoor <goutham.soratoor@hpe.com>
2021-06-09 22:45:09 +0530

    added readme

--- src/main.py
+++ src/main.py
@@ -28,7 +28,7 @@ You don't necessarily need a computer to run commands on your servers.
 I am backdoor to your servers. you can send me a command that you want to run on your server,
 I will execute the commands and return you the result. but before that I should be running on your servers.
 I am open source and free telegram bot. Please clone my github repo to start using me. for more information please
-visit https://www.github.com/grsoratoor
+visit https://github.com/grsoratoor/BackdoorBot
     
 Available Commands
 /start - starts the bot
